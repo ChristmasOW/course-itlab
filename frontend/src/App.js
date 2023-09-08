f6eeaa32543dee2b6535f3fcface903f0401cf57
Vadim Kuzmuk <christmaslife1@gmail.com>
2023-09-09 01:52:42 +0300

    add createBrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
 import Error from './components/Error';
 import Product from './components/Product';
 import Payment from './components/Payment';
+import styled from "styled-components";
+import { createBrowserRouter, Link, Outlet, RouterProvider } from "react-router-dom";
+
+const Ul = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+`;
+
+const Li = styled.li`
+  color: #f0f0f0;
+  font-family: Montserrat;
+  font-size: 20px;
+  margin: 5px 10px;
+`;
+
+let router = createBrowserRouter(
+  [
+    {
+      path: "/", element: <>
+        <div>
+          <Ul>
+            <Li><Link to="/">Home</Link></Li>
+            <Li><Link to="/product">Product</Link></Li>
+            <Li><Link to="/payment">Payment</Link></Li>
+          </Ul>
+        </div>
+        <Outlet />
+      </>,
+      children:
+        [
+          {
+            index: true, element:
+              <>
+                <h1>Home</h1>
+                <p>Homepage content
+                </p>
+              </>
+          },
+          { path: "product/:productId?", element: <Product /> },
+          { path: "payment", element: <Payment /> },
+          { path: "*", element: <Error /> },
+        ],
+      errorElement: <Error />
+    }
+  ]
+)
 
 function App() {
   return (
-    <div>
-      <h1>Router Example</h1>
-      <p>Navigation</p>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/product">Product</Link></li>
-        <li><Link to="/payment">Payment</Link></li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<>
-          <h1>Home</h1>
-          <p>Homepage content</p>
-        </>}
-        />
-        <Route path="/product/:productId?" element={<Product />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
